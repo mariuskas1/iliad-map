5c2ee455bbbd409ce4c350e6ba1469eeb41655b2
Marius Kasparek <marius.kasparek@gmail.com>
2025-03-25 14:32:17 +0100

    basic app structure

---
+++ src/CharacterGraph.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Graph from "react-vis-network-graph";
+
+const CharacterGraph = () => {
+  return(
+    <h1>hello</h1>
+  );
+  
+};
+
+export default CharacterGraph;
